7e23ef7e6b9d4a8e8a52d7d02e8cf0a3970ca34f
Victor <vlinmy06@gmail.com>
2020-02-05 14:56:12 -0800

    add comments for future reference

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Vehicles from "./Vehicles"; // name them capitalized
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello JSX</h1>
+      <Vehicles /> //call using JSX syntax
     );
   }
 }
